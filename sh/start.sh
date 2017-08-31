d992a025bf5745ab01dd3d125486d20b2581ae7c
k0k-1 <k@k0.gg>
2017-08-31 20:48:08 +0900

    [script] overall change

--- sh/start.sh
+++ sh/start.sh
@@ -1,260 +1,251 @@
 #/bin/bash
 
-#                 __       _   _
-#                 \ \     | |_(_)_ __ ___ ___
-#                  > >    | / / | '_ \___(_-<
-#                 /_/     |_\_\_| .__/   /__/
+#             __     _   _
+#             \ \   | |_(_)_ __ ___ ___
+#              > >  | / / | '_ \___(_-<
+#             /_/   |_\_\_| .__/   /__/
+#                         |_|
 
 #       * file name : start.sh
 #       * auther    : kip-s
 #       * url       : https://kip-s.net
-#       * ver       : 2.13
+#       * ver       : 3.00
 
 set -u
 trap exit ERR
 
-WORK_DIR=$(dirname $(dirname $(readlink -f $0)))
-DOTHOME=$HOME/.dot/
+WORKDIR=$(dirname $(dirname $(readlink -f $0)))
+DOTHOME="${HOME}/.dot"
 
-COPYHOME=1
-FLAG_VIM=1
-FLAG_GIT=0
-FLAG_ZSH=1
-FLAG_SSH=0
-FLAG_BIN=1
-FLAG_TMUX=0
-FLAG_EMACS=0
-FLAG_SH=1
+## FLAG ########################################################
 
-ERRMSG=31 # red
-LOGMSG=32 # green
-WRNMSG=33 # yellow
-H1MSG=34  # blue
-H2MSG=35  # purple
+FLAG=("sh" "vim" "zsh" "git" "ssh") # "tmux" )
 
-if [ $FLAG_VIM == 1 ]; then
-  VIM="vim/"
-  CONFIG_VIM="${VIM}vimrc"
-  LINK_CONFIG_VIM=".vimrc"
-  DIR_VIM="${VIM}rc"
-  LINK_DIR_VIM=".vim/rc"
-fi
+################################################################
 
-if [ $FLAG_GIT == 1 ]; then
-  CONFIG_GIT=".gitconfig"
-  LINK_CONFIG_GIT=$CONFIG_GIT
-fi
-
-if [ $FLAG_ZSH == 1 ]; then
-  ZSH="zsh/"
-  CONFIG_ZSH="${ZSH}zshenv"
-  LINK_CONFIG_ZSH=".zshenv"
-  DIR_ZSH="zsh"
-  LINK_DIR_ZSH=".zsh"
-fi
-
-if [ $FLAG_TMUX == 1 ]; then
-  DIR_TMUX="tmux-config"
-  LINK_DIR_TMUX=$DIR_TMUX
-  CONFIG_TMUX=".tmux.conf"
-  LINK_CONFIG_TMUX=$CONFIG_TMUX
-fi
-
-if [ $FLAG_SH == 1 ]; then
-  DIR_SH="sh"
-  LINK_DIR_SH=".sh"
-fi
-
-# \033 = esc
-msg() {
-    if [ $1 == "key" ]; then
-      read -p "press [enter] to ${@:2}." KEY
-    else
-      printf "\033[$1m${@:2}\033[00m\n"
-      sleep 0.1
-    fi
+msg()
+{
+  if [ $1 == "input" ]; then
+    read -p "press [enter] to ${@:2}." KEY
+  else
+    local HEADERTYPE1="|-- [$1] "
+    case $1 in
+      error)
+        local COLOR=31 #RED
+        local HEADER="\n%% ERROR %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n"
+        local FOOTER="\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n"
+        ;;
+      success)
+        local COLOR=32 # GREEN
+        local HEADER=${HEADERTYPE1}
+        local FOOTER=""
+        ;;
+      failed)
+        local COLOR=31 #YELLOW
+        local HEADER=${HEADERTYPE1}
+        local FOOTER=""
+        ;;
+      h1)
+        local COLOR=34 #BLUE
+        local HEADER="\n## WELCOME ####################################################\n"
+        local FOOTER=""
+        ;;
+      h2)
+        local COLOR=35 #PURPLE
+        local HEADER="|-- [msg] "
+        local FOOTER=""
+        ;;
+      log)
+        local COLOR=1
+        local HEADER=${HEADERYPE1}
+        local FOOTER=""
+    esac
+    local HEADER="\033[${COLOR}m${HEADER}"
+    local FOOTER="${FOOTER}\033[00m\n"
+    printf "${HEADER}${@:2}${FOOTER}"
+  fi
 }
 
-switch() {
-    if [ $1 == 1 ]; then
-        msg $LOGMSG "| - [log] $2=on"
-    else
-        msg $LOGMSG "| - [log] $2=off"
-    fi
+setconf ()
+{
+  case $1 in
+    sh)
+      local DIRNAME="sh"
+      local MAKEDIR="bin"
+      local OPTION=("dir")
+      ;;
+    vim)
+      local DIRNAME="vim"
+      local MAKEDIR=".${DIRNAME}"
+      local FILENAME=("vimrc" "${MAKEDIR}/rc")
+      local LINKNAME=(".vimrc" "${MAKEDIR}/rc")
+      local OPTION=("dir" "link")
+      ;;
+    zsh)
+      local DIRNAME="zsh"
+      local MAKEDIR=".cache/${DIRNAME}"
+      local FILENAME=("zshenv" ${DIRNAME})
+      local LINKNAME=(".zshenv" ".${DIRNAME}")
+      local OPTION=("dir" "link")
+      ;;
+    git)
+      local DIRNAME="git"
+      local FILENAME=("gitconfig")
+      local LINKNAME=(".gitconfig")
+      local OPTION=("link")
+      ;;
+    ssh)
+      local DIRNAME="ssh"
+      local MAKEDIR="${DIRNAME}/.pub"
+      local OPTION=("dir")
+      ;;
+    tmux)
+      local DIRNAME="tmux"
+      local FILENAME=(".tmux.conf")
+      local LINKNAME=(".tmux.conf")
+      local OPTION=("link")
+      ;;
+  esac
+
+  for c in ${OPTION[@]}
+  do
+    case $c in
+      dir)
+        local DIRPATH="${WORKDIR}/${MAKEDIR}"
+        if [ -d ${DIRPATH} ]; then
+          mkdir -p ${DIRPATH}
+          msg success "mkdir ${DIRPATH}"
+        else
+          msg failed "${DIRPATH} is already exist. [directory]"
+        fi
+      ;;
+      link)
+        if [ ${#FILENAME[@]} == ${#LINKNAME[@]} ]; then
+        for n in ${FILENAME[@]}
+        do
+          local FILEPATH=(${FILEPATH[@]} "${WORKDIR}/${DIRNAME}/${n}")
+        done
+        for n in ${LINKNAME[@]}
+        do
+          local LINKPATH=(${LINKPATH[@]} "${HOME}/${n}")
+        done
+        fi
+        for (( i = 0; i < ${#FILEPATH[@]}; ++i ))
+        do
+          if [ ! -e ${LINKPATH[$i]} ]; then
+            ln -s ${FILEPATH[$i]} ${LINKPATH[$i]}
+            msg success "link ${FILEPATH[$i]} -> ${LINKPATH[$i]}"
+          else
+            msg failed "${LINKPATH[$i]} is already exist. [symblic link]"
+          fi
+        done
+        unset FILEPATH LINKPATH
+      ;;
+    esac
+  done
 }
 
-cchk(){
-    if [ ! -e $HOME/$2 ]; then
-        case $1 in
-            mkdir)
-                mkdir -p $2
-                msg $LOGMSG "| - [log] make dir '$2' $HOME/$2" ;;
-            ln)
-                ln -s $WORK_DIR/$3 $HOME/$2
-                msg $LOGMSG "| - [log] make symbolic link $HOME/$2" ;;
-            cp)
-                cp -r $2 $HOME/$3
-                msg $LOGMSG "| - [log] copy $2 -> $HOME/$3" ;;
-        esac
+gitinstall()
+{
+  local GITVER="2.13.0-rc1"
+  local GITFN="v${GITVER}.tar.gz"
+  msg h2 "install git-${GITVER}"
+  if type wget >/dev/null 2>&1; then
+    msg log "downloading git-${GITFILE}"
+    wget https://github.com/git/git/archive/${GITFILE} /tmp/${GITFILE}
+    msg log "download completed!-${GITFILE}"
+    cd /tmp/
+    tar -zxf ${GITFILE} ${GITVER}
+    cd /tmp/${GITVER}
+    if type make >/dev/null 2>&1; then
+      make configure
+      ./configure --prefix=usr
+      make all doc info
+      sudo make install
     else
-        msg $WRNMSG "| - [msg] $HOME/$2 is already exist."
+      msg error "your computer 'make' is not installed."
     fi
+    rm -r ${GITFILE} ${GITVER}
+    msg log "removed '${GITFILE}' and '${GITVER}'."
+    cd ${WORKDIR}
+  else
+    msg error "seriously! your computer 'wget' is NOT installed!"
+  fi
 }
 
-case $OSTYPE in
-    msys*)
-        msg $ERRMSG "| - [error]"
-        msg $ERRMSG "| - your environment is $OSTYPE."
-        msg $ERRMSG "| - this script only run for linux."
-        msg "key" "exit"
-        return 2>&- || exit ;;
-    linux*) ;;
+zpluginstall()
+{
+  msg h2 "install zplug"
+  if type git >/dev/null 2>&1; then
+    if [ ! -d ${HOME}/.zplug ]; then
+      export ZPLUG_HOME=$HOME/.zplug
+      msg log "installing zplug..."
+      cchk mkdir ".zplug"
+      git clone https://github.com/zplug/zplug ${ZPLUG_HOME}
+      msg h2 "done"
+    else
+      msg h2 "skip"
+    fi
+  fi
+}
+
+tmuxinit()
+{
+  msg h2 "init submodule"
+  if [ ! -e ${HOME}/.tmux.conf ]; then
+    msg log "installing tmux-config..."
+  if type git >/dev/null 2>&1; then
+    git clone https://github.com/kip-s/${DIR_TMUX} ${WORKDIR}/${DIR_TMUX}
+    cd ${WORKDIR}/${DIR_TMUX} && git submodule init && git submodule update
+    cd ${WORKDIR}/${DIR_TMUX}/vendor/tmux-mem-cpu-load && cmake . && make && sudo make install
+    cd ${WORKDIR}
+    msg h2 "done"
+  else
+    msg h2 "skip"
+  fi
+  fi
+
+  msg h2 "install tpm"
+  if type git >/dev/null 2>&1; then
+  if [ ! -e ${HOME}/.tmux ]; then
+    msg log "installing tpm..."
+    git clone https://github.com/tmux-plugins/tpm ${HOME}/.tmux/plugins/tpm
+    cd ${WORKDIR}
+    msg h2 "done"
+  else
+    msg h2 "skip"
+  fi
+  fi
+}
+
+## MAIN ########################################################
+
+msg h1 "scriptname: $0\n"
+
+case ${OSTYPE} in
+  linux*)
+    ;;
+  *)
+    msg error "Your environment is ${OSTYPE}. This script can run only on linux."
+    msg input "exit"
+    return 2>&- || exit
+    ;;
 esac
 
-msg $H1MSG "\n* » start init.sh!!\n-----------------------------"
-msg "key" "start"
-
-switch $FLAG_VIM "vim"
-switch $FLAG_GIT "git"
-switch $FLAG_ZSH "zsh"
-switch $FLAG_SSH "ssh"
-switch $FLAG_TMUX "tmux"
-switch $FLAG_EMACS "emacs"
-
-if [ $COPYHOME == 1 ]; then
-    if [ ! -e $WORK_DIR ]; then
-        cchk cp $WORK_DIR ".dot"
-        $WORK_DIR = $DOTHOME
-    fi
+if [ ! -e ${WORKDIR} ]; then
+  cp -r ${WORKDIR} ${DOTHOME}
+  ${WORKDIR} = ${DOTHOME}
 fi
-
-cd $HOME/
-msg $LOGMSG "| - [log] cd $HOME"
-
-msg $H2MSG "\n* »» make directory"
-
-if [ $FLAG_VIM == 1 ]; then
-    cchk mkdir ".vim"
-fi
-
-if [ $FLAG_ZSH == 1 ]; then
-    cchk mkdir ".cache/zsh"
-fi
-
-if [ $FLAG_SSH == 1 ]; then
-    cchk mkdir ".ssh"
-    cchk mkdir ".ssh/.pub"
-fi
-
-# sh
-if [ $FLAG_BIN == 1 ]; then
-    cchk mkdir ".bin"
-fi
-
-msg $H2MSG "* »» done!"
-
-GIT_VER="2.13.0-rc1"
-GIT_FN="v$GIT_VER.tar.gz"
 
 if ! type git >/dev/null 2>&1; then
-    if [ $FLAG_BIN == 1 && $FLAG_GIT == 1 ]; then
-        msg $H2MSG "\n* »» install git-$GIT_VER"
-        if type wget >/dev/null 2>&1; then
-            msg $LOGMSG "| - [log] downloading git-$GIT_FN"
-            wget https://github.com/git/git/archive/$GIT_FN /tar/$GIT_FN
-            msg $LOGMSG "| - [log] download completed!-$GIT_FN"
-            cd /tmp/
-            tar -zxf $GIT_FN $GIT_VER
-            cd /tmp/$GIT_VER
-            if type make >/dev/null 2>&1; then
-                make configure
-                ./configure --prefix=usr
-                make all doc info
-                sudo make install
-            else
-                msg $ERRMSG "| - [error] your computer 'make' is not installed."
-            fi
-            rm -r $GIT_FN $GIT_VER
-            msg $LOGMSG "| - [log] removed '$GIT_FN' and '$GIT_VER'."
-            cd $WORK_DIR
-        else
-            msg $ERRMSG "| - [error] seriously! your computer 'wget' is NOT installed!"
-        fi
-    fi
+  gitinstall
 fi
 
-if [ $FLAG_ZSH == 1 ]; then
-    msg $H2MSG "\n* »» install zplug"
-    if type git >/dev/null 2>&1; then
-        if [ ! -d $HOME/.zplug ]; then
-            export ZPLUG_HOME=$HOME/.zplug
-            msg $LOGMSG "| - [log] installing zplug..."
-            cchk mkdir ".zplug"
-            git clone https://github.com/zplug/zplug $ZPLUG_HOME
-            msg $H2MSG "* »» done"
-        else
-            msg $H2MSG "* »» skip"
-        fi
-    fi
-fi
+for f in ${FLAG[@]}
+do
+  msg h2 "start ${f}"
+  setconf ${f}
+  msg h2 "end ${f}\n"
+done
 
-if [ $FLAG_TMUX == 1 ]; then
-    msg $H2MSG "\n* »» init submodule"
-    if [ ! -e $HOME/.tmux.conf ]; then
-        msg $LOGMSG "| - [log] installing tmux-config..."
-        cd $WORK_DIR/$DIR_TMUX && git submodule init && git submodule update
-        cd $WORK_DIR/$DIR_TMUX/vendor/tmux-mem-cpu-load && cmake . && make && sudo make install
-        cd $WORK_DIR
-        msg $H2MSG "* »» done"
-    else
-        msg $H2MSG "* »» skip"
-    fi
-fi
-
-if [ $FLAG_TMUX == 1 ]; then
-    msg $H2MSG "\n* »» install tpm"
-    if type git >/dev/null 2>&1; then
-    if [ ! -e $HOME/.tmux ]; then
-        msg $LOGMSG "| - [log] installing tpm..."
-        git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-        cd $WORK_DIR
-        msg $H2MSG "* »» done"
-    else
-        msg $H2MSG "* »» skip"
-    fi
-    fi
-fi
-
-msg $H2MSG "\n* »» create symbolic link"
-
-# vim
-if [ $FLAG_VIM == 1 ]; then
-    cchk ln $LINK_CONFIG_VIM $CONFIG_VIM
-    cchk ln $LINK_DIR_VIM $DIR_VIM
-fi
-
-# git
-if [ $FLAG_GIT == 1 ]; then
-    cchk ln $LINK_CONFIG_GIT $CONFIG_GIT
-fi
-
-# zsh
-if [ $FLAG_ZSH == 1 ]; then
-    cchk ln $LINK_CONFIG_ZSH $CONFIG_ZSH
-    cchk ln $LINK_DIR_ZSH $DIR_ZSH
-fi
-
-# tmux
-if [ $FLAG_TMUX == 1 ]; then
-  cchk ln $LINK_CONFIG_TMUX $CONFIG_TMUX
-  tmux source-file $HOME/.tmux.conf
-fi
-
-# sh
-if [ $FLAG_BIN == 1 ]; then
-  cchk ln $LINK_DIR_SH $DIR_SH
-fi
-
-msg $H2MSG "* »» done!"
-
-msg $H1MSG "\n-----------------------------\n* » all done!!\n"
+################################################################
