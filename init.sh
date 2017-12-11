9025fe2400c8f4689237200215bdef1a814fcee3
k0k-1 <k@k0.gg>
2017-12-11 18:42:00 +0900

    [sh] add init.sh

---
+++ init.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+CDIR=$(dirname $(readlink -f $0))
+RED=31
+
+INITSH="start.sh"
+
+msg() {
+		printf "\033[$1m${@:2}\033[00m\n"
+}
+
+if [ -e $CDIR/sh/$INITSH ]; then
+		bash $CDIR/sh/$INITSH
+else
+		msg $RED "[error] '${INITSH}' doesn't exist."
+		return 2>&- || exit
+fi
