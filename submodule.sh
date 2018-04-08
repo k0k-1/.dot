8241bd7eb6ce242f8fbe1a3b69399243d39bede9
k0k-1 <k@k0.gg>
2018-04-08 19:17:10 +0900

    [sh] add shell script to initialize submodule

---
+++ submodule.sh
@@ -0,0 +1,3 @@
+cd $HOME/.dot && git submodule init && git submodule update
+cd $HOME/.dot/tmux && git submodule init && git submodule update
+#;cd $HOME/.dot/tmux/vendor/tmux-mem-cpu-load && cmake . && make && sudo make install
