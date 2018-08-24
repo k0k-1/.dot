bdc7937c079b0aad18834d90a3e6cff44aa9d8cf
k0k-1 <k@k0.gg>
2018-08-24 19:20:27 +0900

    [sh] change hardtab to softtab

--- zsh/rc/plugin/color.sh
+++ zsh/rc/plugin/color.sh
@@ -3,6 +3,6 @@
 printf "FOREGROUND: 38;05;\${COLOR_NUMBER}\nBACKGROUND: 48;05;\${COLOR_NUMBER}\n"
 
 for j in {3..4}; do for i in {0..255}; do
-	if [ $(( ${i}%16 )) -eq 0 ];then printf "\n"; fi
-	printf "\x1b[${j}8;05;${i}m%4d" ${i}
+  if [ $(( ${i}%16 )) -eq 0 ];then printf "\n"; fi
+  printf "\x1b[${j}8;05;${i}m%4d" ${i}
 done; done
